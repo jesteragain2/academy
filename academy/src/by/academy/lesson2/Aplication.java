f720a74c8c18ac471ac1bda5621a1ff5be6dfe66
Jesteragain2 <anotheraddress4@gmail.com>
2020-10-10 15:18:07 +0300

    1423

--- academy/src/by/academy/lesson2/Aplication.java
+++ academy/src/by/academy/lesson2/Aplication.java
@@ -3,8 +3,10 @@ package by.academy.lesson2;
 public class Aplication {
 
 	public static void main(String[] args) {
-		//1423
-
+		Cat bor=new Cat();
+		
+		bor.setMoney(12.45);		
+		System.out.println(bor.getMoney());
 	}
-
+	
 }
