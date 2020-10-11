9d77bcee011452fd0417c827e3632b48eaafb0b9
jesteragain2 <72651771+jesteragain2@users.noreply.github.com>
2020-10-11 22:49:53 +0300

    homework1

--- academy/src/by/academy/homework/homework1/Application.java
+++ academy/src/by/academy/homework/homework1/Application.java
@@ -1,5 +1,21 @@
 package by.academy.homework.homework1;
 
 public class Application {
-
+	public static void main(String ...args){
+		
+		Cat cat1=new Cat();
+		Cat bor=new Cat("Boris");
+		
+		bor.eat();
+		bor.sleep();
+		bor.walk();
+		
+		cat1.grow();
+		System.out.println("Возраст кота '"+cat1.nickname+"' (в годах): "+cat1.age);
+		cat1.grow();
+		System.out.println("Возраст кота '"+cat1.nickname+"' (в годах): "+cat1.age);
+		cat1.grow();
+		System.out.println("Возраст кота '"+cat1.nickname+"' (в годах): "+cat1.age);
+		
+	}
 }
--- academy/src/by/academy/homework/homework1/Cat.java
+++ academy/src/by/academy/homework/homework1/Cat.java
@@ -1,5 +1,33 @@
 package by.academy.homework.homework1;
 
 public class Cat {
-
+	
+	int age;
+	String nickname="безимянный";
+	
+	public void grow() {
+		this.age=age+1;
+	}
+	
+	public void sleep() {
+		System.out.println("Кот "+nickname+" спит.");
+	}
+	
+	public void eat() {
+		System.out.println("Кот "+nickname+" ест.");
+	}
+	
+	public void walk() {
+		System.out.println("Кот "+nickname+" гуляет.");
+	}
+	
+	public Cat() {
+		System.out.println("Кот создан.");
+	}
+	
+	public Cat(String nickname) {
+		this.nickname=nickname;
+		System.out.println("Кот "+nickname+" создан.");
+	}
+	
 }
