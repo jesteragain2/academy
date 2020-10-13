b3d3dc3607ebef51ff1c678972856d25b8eef20a
jesteragain2 <72651771+jesteragain2@users.noreply.github.com>
2020-10-13 13:10:48 +0300

    Homework 1 complite

--- academy/src/by/academy/homework/homework1/Application.java
+++ academy/src/by/academy/homework/homework1/Application.java
@@ -17,5 +17,20 @@ public class Application {
 		cat1.grow();
 		System.out.println("Возраст кота '"+cat1.nickname+"' (в годах): "+cat1.age);
 		
+		bor.setMoney(13.23);
+		System.out.println("У кота с именем "+bor.nickname+" есть "+bor.getMoney()+"$.");		
+	
+		bor.setInitials('B');
+		System.out.println("У кота с именем "+bor.nickname+" первая буква клички "+bor.getInitials()+".");		
+		
+		bor.setIsHomeAnimal(true);
+		bor.getIsHomeAnimal();
+		if (bor.isHomeAnimal==true) {
+			System.out.println("Кот "+bor.nickname+" домашнее животное.");
+		}else {
+			System.out.println("Кот "+bor.nickname+" дикое животное.");
+		}
+		
 	}
 }
+ 
--- academy/src/by/academy/homework/homework1/Cat.java
+++ academy/src/by/academy/homework/homework1/Cat.java
@@ -4,6 +4,9 @@ public class Cat {
 	
 	int age;
 	String nickname="безимянный";
+	double money;
+	char initials;
+	boolean isHomeAnimal;
 	
 	public void grow() {
 		this.age=age+1;
@@ -28,6 +31,30 @@ public class Cat {
 	public Cat(String nickname) {
 		this.nickname=nickname;
 		System.out.println("Кот "+nickname+" создан.");
+	}
+	
+	public void setMoney(double money) {
+		this.money=money;
+	}
+	
+	public double getMoney() {
+		return money;
+	}
+	
+	public void setInitials(char initials) {
+		this.initials=initials;
+	}
+	
+	public char getInitials() {
+		return initials;
+	}
+	
+	public void setIsHomeAnimal(boolean isHomeAnimal) {
+		this.isHomeAnimal=isHomeAnimal;
+	}
+	
+	public boolean getIsHomeAnimal() {
+		return isHomeAnimal;
 	}
 	
 }
